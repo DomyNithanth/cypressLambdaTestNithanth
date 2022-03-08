ef52c4174d7844b48d5a9d7fbc005c53351409e9
DomyNithanth <nithanth07@gmail.com>
2022-03-08 16:19:00 +0000

    lambdatestv6

--- cypress/tests/2-scenario2.spec.ts
+++ cypress/tests/2-scenario2.spec.ts
@@ -2,7 +2,7 @@ import { ScenarioSteps } from "../steps/scenario/scenario_steps";
 
 describe("Scenrio 2: A user should be able to", () => {
 
-    it('change the default value 15 range slider to 95', function () {
+    it('input form, submit it and validate it', function () {
         new ScenarioSteps().navigateToSeleniumPlayground()
             .changePort()
             .clickInputFormSubmit()
